e84e2db85df41eed45c19c74f2d9ee641bd8496b
Dilsher <dilsherb@gmail.com>
2023-05-08 16:57:17 +0300

    recipe component created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import Recipe from './components/recipe';
 
 function App() {
-  const apiKey = "4beac98980dc421e940d8dbca15cb860";
-  const url = `https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=${apiKey}`;
-
+  
   const [recipes, setRecipes] = useState([])
   useEffect(()=>{
     getRecipes();
   },[]);
   
   const getRecipes = async () => {
-    const Response = await fetch(url);
+    const apiKey = "4beac98980dc421e940d8dbca15cb860";
+    const Response = await fetch(`https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=${apiKey}`);
     const data = await Response.json();
     setRecipes(data.products);
   }
+  console.log(recipes)
   return (
     <div className="App">
-      
+      <Recipe />
     </div>
   );
 }
---
+++ src/components/recipe.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Recipe = () => {
+  return (
+    <div>
+      <div>title: title</div>
+      <div>Image: imgage</div>
+    </div>
+  )
+}
+
+export default Recipe
