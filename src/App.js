1b5e1e4da32b9929f764fdc651d09eb79e72a136
Dilsher <dilsherb@gmail.com>
2023-05-08 16:00:21 +0300

    api data fetched

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const apiKey = "4beac98980dc421e940d8dbca15cb860";
+  const url = `https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=${apiKey}`;
+
+  const [recipes, setRecipes] = useState([])
+  useEffect(()=>{
+    getRecipes();
+  },[]);
+  
+  const getRecipes = async () => {
+    const Response = await fetch(url);
+    const data = await Response.json();
+    setRecipes(data.products);
+  }
   return (
     <div className="App">
       
